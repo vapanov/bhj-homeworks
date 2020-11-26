192acff1e9ff634c61f8b97d11b811e324668c7a
vapanov <valentin.panov@me.com>
2020-11-27 01:25:17 +0300

    2тв done

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,20 @@
 "use strict";
 
-//id cookie id clicker__counter clicker__speed 
+let clickCounter = document.getElementById("clicker__counter").textContent;
+let clickSpeed = document.getElementById("clicker__speed").textContent;
 
-let clickCounter = 0;
-let clickSpeed = 0;
+const element = document.getElementById("cookie"); // наша печенька
+let prevDate = new Date();  // переменная для скорости клика
 
+element.onclick = function() {
+   clickCounter++;
+   if (this.width == "200") {this.width = "210";} 
+   else {this.width = "200";}
+
+   clickSpeed = (1000 / (Date.now() - prevDate)).toFixed(2); // посчитаем скорость клика и сократим до 2х после запятой
+   prevDate = Date.now();
+   
+   document.getElementById("clicker__counter").textContent = clickCounter;
+   document.getElementById("clicker__speed").textContent = clickSpeed;
+
+};
