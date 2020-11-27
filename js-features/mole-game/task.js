762e0f70a040c2de10c08b9aef28cfca41dddf8b
vapanov <valentin.panov@me.com>
2020-11-27 23:18:20 +0300

    all 3 done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,29 +1,28 @@
 "use strict";
 
-let winCount, looseCount = 0;
+// не понимаю, мы вызываем этот скрипт при каждом клике?
+// или один раз вызываем, создаём обработчики и они уже включаются при кликах?
 
-//let holeActiveClasses = document.getElementById("hole1").class;
+let winCount = 0, looseCount = 0; // инициируем переменные удач и промахов
 
-   for (let i = 1, activeHole, element; i < 10; i++) {
-      activeHole = "hole" + i;
+for (let i = 1, element; i < 10; i++) {
+   element = document.getElementById(`hole${i}`); // ловим элемент с индексом
 
-      element = document.getElementById(activeHole);
-
-      element.onclick = function() {
-         if (this.class == "hole hole_has-mole") {
-            winCount++;
-         } else {
-            looseCount++;
-         }
+   element.onclick = function() { // обработчик нажатия для элемента
+      if (this.className == "hole hole_has-mole") {
+         winCount++; // плюсуем
+         document.getElementById("dead").textContent = winCount; // записываем на страницу результат
+         if (winCount == 10) {alert("WIN!"); reset();} // победа или нет?
+      } else {
+         looseCount++;
+         document.getElementById("lost").textContent = looseCount;
+         if (looseCount == 10) {alert("LOOSE!"); reset();}
       }
    }
+}
 
-   if (winCount == 10) {alert("WIN!"); reset();}
-   if (looseCount == 10) {alert("LOOSE!"); reset();}
-
+function reset() { // обнуляем счётчики и перерисовываем их
+   winCount = looseCount = 0;
    document.getElementById("dead").textContent = winCount;
    document.getElementById("lost").textContent = looseCount;
-
-function reset() {
-   winCount = looseCount = 0;
 }
