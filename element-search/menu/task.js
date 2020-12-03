90c6636714dfdaad79fd3e1066663a93b854e8f6
vapanov <valentin.panov@me.com>
2020-12-03 23:45:27 +0300

    1.2 menu and modal done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,36 @@
 'use strict';
 
-console.log();
-//      debugger;
+// console.log();
+// debugger;
 
 // Используя цикл зарегистрируйте обработчики события click на элементах с классом menu__link
-
 const elementsMenuLink = Array.from(document.querySelectorAll('.menu__link'));
-for (let i = 0, element; i < elementsMenuLink.length; i++) {
-   element = elementsMenuLink[i];
-   element.onclick = function () {
-      //this.closest('.menu').className = 'menu__sub menu_active';
-      console.log(this); // именно там, где оно нужно, this не работает
-      let descendMenu = Array.from(this.querySelectorAll('.menu'));
-      console.log(descendMenu);
-   };
-}
+
+// объявляем переменную последнее активное меню
+let lastActiveMenu;
+
+for (let i = 0; i < elementsMenuLink.length; i++) {
+// обработчик нажатия на пункт меню
+   elementsMenuLink[i].onclick = function () {
 
 // Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active
-// elemA.contains(elemB) вернёт true, если elemB находится внутри elemA (elemB потомок elemA) или когда elemA==elemB.
-
+// ищем ближайшее вложенное меню
+      const elementMenuSub = this.closest('.menu__item').querySelector('.menu_sub');
+// если таковое есть, то активируем его
+      if (elementMenuSub) {
+// если какое-то меню уже активировано, и это не обрабатываемое меню - деактивировать
+         if (elementMenuSub != lastActiveMenu && lastActiveMenu) {
+            lastActiveMenu.classList.remove('menu_active');
+         }
+// переключаем активатор
+         elementMenuSub.classList.toggle('menu_active');
+// присваиваем последнемий активный блок меню 
+         lastActiveMenu = elementMenuSub;
 
 // Запрещайте переход по ссылке для тех, что имеют вложенное меню. 
+         return false;
+      }
 // Остальные пункты меню должны без помех переводить на соответствующие страницы.
+
+   };
+}
