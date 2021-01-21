c1873d75f94d8939805169379aa57ef9d57a8c9d
vapanov <valentin.panov@me.com>
2021-01-22 00:44:59 +0300

    a-r 1 started

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,33 @@
+'use strict';
+//console.log();
+
+const imgLoader = document.querySelector('img.loader');
+const divItems = document.querySelector('div#items');
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+xhr.setRequestHeader('Content-Type','application/json');
+xhr.send();
+xhr.addEventListener('readystatechange', function() {
+   if (xhr.readyState === 4) {
+      
+      const currencyResponce = Object.keys(xhr.response).map(element => ({
+          CharCode: CharCode,
+          Value: Value
+      }));
+
+//      currencyResponce.forEach((element) => addCurrencyTemplate(element));
+
+      imgLoader.classList.remove('loader_active');
+   };
+});
+
+
+function addCurrencyTemplate (currency) {
+   divItems.insertAdjacentHTML('beforeend',
+      `<div class="item">
+         <div class="item__code">${currency.CharCode}</div>
+         <div class="item__value">${currency.Value}</div>
+         <div class="item__currency">руб.</div>
+      </div>`);
+}
